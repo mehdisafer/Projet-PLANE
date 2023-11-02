18aabdc2e8d653d4da5c86d8b0ebed015007e1f9
Jhonatan Caldeira <jhonatansocaldeira@gmail.com>
2023-11-02 16:43:57 +0100

    New form

---
+++ pages/formulaire.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from components.readfromsql import get_gender, get_class_type, get_customer_type, get_travel_type
+
+
+def evaluation():
+    return range(6)
+
+
+st.selectbox("Genre", [gender[0] for gender in get_gender()])
+st.selectbox("Type de Client", [customer_type[0]
+             for customer_type in get_customer_type()])
+st.selectbox("Classe", [class_type[0] for class_type in get_class_type()])
+st.selectbox("Type de Voyage", [travel_type[0]
+             for travel_type in get_travel_type()])
+
+
+st.text_input("L'âge:")
+st.text_input("Distance de vol:")
+st.text_input("Retard de départ en minutes:")
+st.text_input("Retard d'arrivée en minutes:")
+
+st.radio('Service Wifi en Vol:', evaluation(), horizontal=True)
+st.radio('Heure de départ/arrivée pratique:', evaluation(), horizontal=True)
+st.radio('Facilité de réservation en ligne:', evaluation(), horizontal=True)
+st.radio('Emplacement de la porte:', evaluation(), horizontal=True)
+st.radio('Nourriture et boissons:', evaluation(), horizontal=True)
+st.radio('Embarquement en ligne:', evaluation(), horizontal=True)
+st.radio('Confort du siège:', evaluation(), horizontal=True)
+st.radio('Divertissement à bord:', evaluation(), horizontal=True)
+st.radio('Service à board:', evaluation(), horizontal=True)
+st.radio('Service de chambre pour le jamble:', evaluation(), horizontal=True)
+st.radio('Gestion des bagages:', evaluation(), horizontal=True)
+st.radio('Service d\'enregistrement:', evaluation(), horizontal=True)
+st.radio('Service en vol:', evaluation(), horizontal=True)
+st.radio('Propreté:', evaluation(), horizontal=True)
